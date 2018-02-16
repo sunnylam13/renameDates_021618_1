e005fde41eada69f5304271c0bb1257b30e51018
Sunny Lam <sunny.lam@gmail.com>
2018-02-16 16:25:25 -0500

    revised the code

--- renameDates/renameDates_021618_1.py
+++ renameDates/renameDates_021618_1.py
@@ -5,3 +5,25 @@
 # USAGE
 # xxxx
 
+import shutil, os, re
+
+#####################################
+# REGEX
+#####################################
+
+# create a regex that matches files with the American date format
+
+datePattern = re.compile(r'''
+		(
+		^(.*?) # all text before the date
+		((0|1)?\d)- # one or two digits for the month
+		((0|1|2|3)?\d)- # one or two digits for the day
+		((19|20)\d\d) # four digits for the year
+		(.*?)$ # all text after the date
+		)
+	''', re.VERBOSE)
+
+#####################################
+# END REGEX
+#####################################
+
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
-	'packages': ['zipfile'],
+	'packages': ['shutil, os, re'],
 	'scripts': [],
 	'name': 'Change American to European Dates'
 }
