770100dc9717ccddeeca2d7b8e5e40e833a4cd91
Sunny Lam <sunny.lam@gmail.com>
2018-02-17 14:25:35 -0500

    revised code

--- renameDates/renameDates_021618_1.py
+++ renameDates/renameDates_021618_1.py
@@ -25,6 +25,34 @@ datePattern = re.compile(r'''
 # END REGEX
 #####################################
 
+#####################################
+# FILE PROCESSING
+#####################################
+
+# Loop over the files in the working directory
+
+for amerFilename in os.listdir('.'):
+	mo = datePattern.search(amerFilename)
+
+	# skip files without a date
+	
+	if mo == None:
+		continue
+
+	# get the different parts of the filename
+	# the groups are in the order of () in the regex statement used (see `datePattern` in REGEX)
+	# see notes on "Nested Brackets of Groups" on counting brackets (python-notes-051416/regex-py3-090317-1.md)
+	
+	beforePart = mo.group(1)
+	monthPart = mo.group(2)
+	dayPart = mo.group(4)
+	yearPart = mo.group(6)
+	afterPart = mo.group(8)
+
+#####################################
+# END FILE PROCESSING
+#####################################
+
 # TODO:  Loop over the files in the working directory
 # TODO:  skip files without a date
 # TODO:  get the different parts of the filename
